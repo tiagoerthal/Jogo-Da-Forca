b289f6f09972e10fe8218d6b596ca9195642984d
tiago <tiagoeweber@gmail.com>
2025-03-25 19:51:21 -0300

    implementaçâo funcionalidade de exibição da dica de palavra

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -4,20 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("Jogo Da Forca");
-            Console.WriteLine("----------------------------------------");
+           while (true)
+            {
+                string palavrasecreta = "Melancia";
+
+                char[] letrasEncontradas = new char[palavrasecreta.Length];
+
+                for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
+                {
+                    // acessar o array no indice 0
+                    letrasEncontradas[caractere] = '_';
+
+                }
+
+                string dicaDaPalavra = string.Join(" ", letrasEncontradas);
 
 
 
+                Console.Clear();
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Jogo Da Forca");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Palavra secreta: " + dicaDaPalavra);
+                Console.WriteLine("----------------------------------------");
 
-            Console.Write("Digite Uma Letra:  ");
-            char chute = Console.ReadLine()[0];  // char utiliza apenas 1 caractere que o usuaria digita
 
-            Console.WriteLine(chute);
+                Console.Write("Digite Uma Letra:  ");
+                char chute = Console.ReadLine()[0];  // char utiliza apenas 1 caractere que o usuaria digita
 
-            Console.ReadLine();
+                Console.WriteLine(chute);
+
+                Console.ReadLine();
+            }
         }
     }
 }
